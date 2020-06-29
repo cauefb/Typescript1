5bdb8e4c6ceb0e84e33a472ecaea86d04264652d
cauê ferreira baraúna <kau3hc@hotmail.com>
2020-06-29 13:55:38 -0300

    aula 4

--- app/ts/controllers/NegociaController.ts
+++ app/ts/controllers/NegociaController.ts
@@ -5,6 +5,7 @@ class NegociacaoController {
     private _inputValor: HTMLInputElement;
     private _negociacoes = new Negociacoes();
     private _negociacoesView = new NegociacoesView('#negociacoesView');
+    private _mensagemView = new MensagemView('#mensagemView');
 
 
     constructor() {
@@ -29,6 +30,7 @@ class NegociacaoController {
 
           // depois de adicionar, atualiza a view novamente para refletir os dados
           this._negociacoesView.update(this._negociacoes);
+          this._mensagemView.update('Negociação adicionada com sucesso')
        
     }
 }
